3f59f44baec3575d6025a27ca1df91f7ea3b63e0
Oak Barrett <1195118+oakbarrett@users.noreply.github.com>
2024-10-03 17:41:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,4 +71,4 @@ if ingredients_list:
 # New section to display fruityvice nutrion information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
