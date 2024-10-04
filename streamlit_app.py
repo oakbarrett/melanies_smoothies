43b696b41d5c8ea56fdd7347a49fe2f95502e1f4
Oak Barrett <1195118+oakbarrett@users.noreply.github.com>
2024-10-04 08:57:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
 st.dataframe(pd_df)
 st.stop()
 
